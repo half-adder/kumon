1a5a4f724c5bcfbe104149d459d0d2bb8a80d0db
Sean Johnsen <seanjohnsen96@gmail.com>
2018-07-03 15:46:29 -0400

    Added links to student detail in student-list view

--- kumon_student_db/student_registration/urls.py
+++ kumon_student_db/student_registration/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from kumon_student_db.student_registration.views import StudentList
+from kumon_student_db.student_registration import views
 
 app_name = "student_registration"
 urlpatterns = [
-    path("students/", StudentList.as_view()),
+    path("students/", views.StudentList.as_view(), name='student-list'),
+    path("students/<int:pk>/", views.StudentDetail.as_view(), name='student-detail'),
 ]
--- kumon_student_db/student_registration/views.py
+++ kumon_student_db/student_registration/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from kumon_student_db.student_registration import models
 
 
 class StudentList(ListView):
     model = models.Student
+
+
+class StudentDetail(DetailView):
+    model = models.Student
+
+    def get_object(self, queryset=models.Student.objects.all()):
+        return super().get_object()
