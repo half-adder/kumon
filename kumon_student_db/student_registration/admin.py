d138378553ff4c7d2d9ce89e4512ed5a6d93dfda
Sean Johnsen <seanjohnsen96@gmail.com>
2018-07-03 14:58:35 -0400

    register student_registration models with admin interface

--- kumon_student_db/student_registration/admin.py
+++ kumon_student_db/student_registration/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from kumon_student_db.student_registration import models
 
 # Register your models here.
+admin.site.register(models.MonthlyCost)
+admin.site.register(models.WhyChoice)
+admin.site.register(models.HowChoice)
+admin.site.register(models.Parent)
+admin.site.register(models.Student)
+
