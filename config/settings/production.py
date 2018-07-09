68b8d0de16c1b756335517c56b65cbc9b44837df
Sean Johnsen <seanjohnsen96@gmail.com>
2018-07-09 10:10:30 -0400

    Try again to fix allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,8 +9,12 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
-    "DJANGO_ALLOWED_HOSTS", default=["hoover-kumon.seanjohnsen.com", "http://vast-ridge-13188.herokuapp.com/"]
+    "DJANGO_ALLOWED_HOSTS", default=["hoover-kumon.seanjohnsen.com"]
 )
+ALLOWED_HOSTS += [
+    'hoover-kumon.seanjohnsen.com',
+    'vast-ridge-13188.herokuapp.com',
+]
 
 # DATABASES
 # ------------------------------------------------------------------------------
