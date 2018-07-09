2662be5d81132641e940b5ea87269da6ad1c5821
Sean Johnsen <seanjohnsen96@gmail.com>
2018-07-09 09:59:11 -0400

    add heroku to allowed hosts manually in config file

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
-    "DJANGO_ALLOWED_HOSTS", default=["hoover-kumon.seanjohnsen.com"]
+    "DJANGO_ALLOWED_HOSTS", default=["hoover-kumon.seanjohnsen.com", "http://vast-ridge-13188.herokuapp.com/"]
 )
 
 # DATABASES
