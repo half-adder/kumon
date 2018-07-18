c45523693e8adcd071b2a160e7780c81c03a8055
Sean Johnsen <seanjohnsen96@gmail.com>
2018-07-18 17:15:11 -0400

    make student list view root view

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
+from django.shortcuts import redirect
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.views import defaults as default_views
+
+from kumon_student_db.student_registration import views
 
 urlpatterns = [
     # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -16,6 +18,7 @@ urlpatterns = [
     path("users/", include("kumon_student_db.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("", views.StudentList.as_view()),
     path(
         "registration/",
         include(
